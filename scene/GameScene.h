1ca77fb30eafa3582c1ed692220e6825524ef9d2
K022G1036@g.neec.ac.jp <K022G1036@g.neec.ac.jp>
2023-04-20 16:31:19 +0900

    01_02_確認課題

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,6 +8,7 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
@@ -48,4 +49,26 @@ private: // メンバ変数
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	//スプライト
+	Sprite* sprite_ = nullptr;
+
+	// モデル
+	Model* model_ = nullptr;
+	// ワールド
+	WorldTransform worldTransform_;
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	// サウンドデータハンドル
+	uint32_t soundDataHandle_ = 0;
+	// 音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+
+    float inputFloat3[3]={0,0,0};
+
+	// デバッグカメラ
+	DebugCamera* debugCamera_ = nullptr;
 };
